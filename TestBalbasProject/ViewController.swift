6466e5c41e1f6baff2737eac1916cc4e03b89627
yuki-h29 <yuki.hirano29@gmail.com>
2024-10-13 10:16:41 +0900

    Initial Commit

---
+++ TestBalbasProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestBalbasProject
+//
+//  Created by 平野裕貴 on 2024/10/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TestBalbasProjectTests/TestBalbasProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestBalbasProjectTests.swift
+//  TestBalbasProjectTests
+//
+//  Created by 平野裕貴 on 2024/10/13.
+//
+
+import Testing
+@testable import TestBalbasProject
+
+struct TestBalbasProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
